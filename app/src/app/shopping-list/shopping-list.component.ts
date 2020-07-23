b4217fc6570e9a5b27588ba2a1cf555648cee440
antergonzo <gonzoibox@gmail.com>
2020-07-23 16:09:07 +0300

    selecting state

--- app/src/app/shopping-list/shopping-list.component.ts
+++ app/src/app/shopping-list/shopping-list.component.ts
@@ -1,32 +1,24 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
-  private igChangeSub: Subscription;
+export class ShoppingListComponent implements OnInit {
+  ingredients: Observable<{ingredients: Ingredient[]}>;
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService,
+              private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) { }
 
-  ngOnInit(): void {
-    this.ingredients = this.shoppingListService.getIngredients();
-    this.igChangeSub = this.shoppingListService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    )
+  ngOnInit() {
+    this.ingredients = this.store.select('shoppingList');
   }
   onEdit(index: number) {
     this.shoppingListService.startedEditing.next(index);
-  }
-
-  ngOnDestroy(): void {
-    this.igChangeSub.unsubscribe();
   }
 }
