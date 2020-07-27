6c8c74c2c8163a60ca230329e20fc469d32e1cf9
gonzoibox <gonzoibox@gmail.com>
2020-07-27 14:38:05 +0300

    multiple actions

--- app/src/app/recipes/recipe.service.ts
+++ app/src/app/recipes/recipe.service.ts
@@ -3,25 +3,16 @@ import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Subject } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
 
 @Injectable()
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    constructor(private shoppingListService: ShoppingListService) {};
-
-    // private recipes: Recipe[] = [
-    //     new Recipe(
-    //         'Schnitzel',
-    //         'A super tasty Schnitzel',
-    //         'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
-    //         [new Ingredient('Meat', 1), new Ingredient('French Fries', 10)]),
-    //     new Recipe(
-    //         'Burger',
-    //         'An awesome Burger King',
-    //         'https://cdn.pixabay.com/photo/2015/04/20/13/25/burger-731298_1280.jpg',
-    //         [new Ingredient('Meat', 1), new Ingredient('Buns', 1)])
-    //   ];
+    constructor(private shoppingListService: ShoppingListService,
+                private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+    ) {};
 
     private recipes: Recipe[] = [];
     
@@ -39,7 +30,7 @@ export class RecipeService {
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
-        this.shoppingListService.addIngredients(ingredients);
+        this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
     }
 
     recipesChangedEvent() {
