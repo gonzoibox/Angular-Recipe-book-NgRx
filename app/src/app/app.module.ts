e8a36ed2fd93870e24175443df6c363359c0de3e
gonzoibox <gonzoibox@gmail.com>
2020-07-30 19:39:27 +0300

    Add REDUX - the Store Devtools

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { CoreModule } from './core.module';
 import * as fromApp from '../app/store/app.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth/auth.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,7 @@ import { AuthEffects } from './auth/auth.effects';
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     SharedModule,
     CoreModule
   ],
